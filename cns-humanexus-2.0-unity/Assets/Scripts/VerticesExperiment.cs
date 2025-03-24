90a2432c8d32cf0440d177f7b3d33dfa988bcbf2
peterkienle <126619321+pmkienle@users.noreply.github.com>
2025-03-24 09:23:16 -0400

    Added cubes with images
    Images are 600x600 png.

--- cns-humanexus-2.0-unity/Assets/Scripts/VerticesExperiment.cs
+++ cns-humanexus-2.0-unity/Assets/Scripts/VerticesExperiment.cs
@@ -7,31 +7,39 @@ public class VerticesExperiment : MonoBehaviour
 
     public GameObject icosphere;
     public GameObject ball;
+
+    public float resizeFactor;
     Vector3[] vertices;
     static private List<Vector3> verticesDone = new List<Vector3>();    //list is used to omit multiple similar vertices
     static private List<GameObject> clones = new List<GameObject>();    //all clones are in here
     private Mesh mesh;
+    //public Transform target;
+
 
     void Start()
     {
-        mesh = GetComponent<MeshFilter>().mesh;
-        vertices = mesh.vertices;
+        mesh = GetComponent<MeshFilter>().mesh; // extract mesh from object
+        vertices = mesh.vertices;               // enter all vertices of mesh into array
         GameObject clone;
         int vertexCounter = 1;
-        Debug.Log("vertices in array: " + vertices.Length);
+       
+        Debug.Log("vertices in object: " + vertices.Length);
 
         foreach (Vector3 vertex in vertices)
         {
             if (!verticesDone.Contains(vertex))
             {
                 clone = Instantiate(ball, vertex, Quaternion.identity);   // make clone
-                clone.transform.parent = icosphere.transform;               // into parent
+                clone.transform.parent = transform;               // into parent
+                clone.transform.LookAt(transform);
                 verticesDone.Add(vertex);                                   // add vertex to list so we can check for dups
                 clones.Add(clone);                                          // keep list of created game objects
                 Debug.Log("(" + vertexCounter + ")" + vertex);
                 vertexCounter++;
             }
         }
+        GetComponent<Renderer>().enabled = false;       // hide icosphere
+        ball.GetComponent<Renderer>().enabled = false;  // hide ball
     }
 
 
@@ -59,6 +67,12 @@ public class VerticesExperiment : MonoBehaviour
                 icosphere.GetComponent<Renderer>().enabled = true;
             }
         }
+
+        icosphere.transform.Rotate(0, 10 * Time.deltaTime, 0);  // constant rotation
+        float resizeValue = 1 + (resizeFactor * Time.deltaTime);
+      
+        ResizeCloud(resizeValue);
+
     }
 
     void ResizeCloud(float factor)
